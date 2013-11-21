5476a1e338be3ad24c0c3f59fb7f74ff90701608
michael <michael@unomena.com>
2013-11-21 18:27:55 +0200

    changed flatpage admin etc around

--- src/app/models.py
+++ src/app/models.py
@@ -3,3 +3,7 @@ Created on 21 Oct 2013
 
 @author: michael
 '''
+#from preferences.models import Preferences
+
+#class SitePreferences(Preferences):
+#    __module__ = 'preferences.models'
--- src/app/root/models.py
+++ src/app/root/models.py
@@ -3,7 +3,3 @@ Created on 21 Oct 2013
 
 @author: michael
 '''
-#from preferences.models import Preferences
-
-#class SitePreferences(Preferences):
-#    __module__ = 'preferences.models'
