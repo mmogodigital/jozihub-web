083539e362e4229877aed75e175311dff5f46e90
sanet <sanet@unomena.com>
2013-11-28 13:21:57 +0200

    Added post a job template.

--- src/app/jobs/urls.py
+++ src/app/jobs/urls.py
@@ -17,5 +17,12 @@ urlpatterns = patterns('',
         ),
         name='job_detail'
     ),
+
+    url(r'^post/job/$',
+        generic_views.TemplateView.as_view(
+                template_name='jobs/post_job.html',
+        ),
+        name='post_job'
+    ),
                        
 )
