65e6a307b652d3776e98366acaf18c51c829cada
Christina Maxwell <christina@unomena.com>
2014-01-22 14:27:00 +0200

    Reference the tunobase events model

--- src/app/events/views.py
+++ src/app/events/views.py
@@ -6,18 +6,19 @@ Created on 25 Nov 2013
 from django.views import generic as generic_views
 
 from tunobase.core import utils as core_utils
+from tunobase.core import models
 
-from app.events import models
 
 class Events(generic_views.ListView):
-    
+
     def get_queryset(self):
         return models.Event.objects.permitted()
-    
+
+
 class EventDetail(generic_views.DetailView):
-    
+
     def get_object(self):
         return core_utils.get_permitted_object_or_404(
-            models.Event, 
+            models.Event,
             slug=self.kwargs['slug']
         )
