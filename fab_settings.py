8b410c954262b3016bb8c8208e396525d22597e8
Christina Maxwell <christina@unomena.com>
2014-01-22 08:47:34 +0200

    Update fab settings

--- fab_settings.py
+++ fab_settings.py
@@ -1,6 +1,6 @@
 import getpass
 
-APP_NAME = 'app'
+APP_NAME = 'jozihub'
 
 REPO_PATH = 'praekelt/jozihub'
 
@@ -14,12 +14,45 @@ HTTPS_PORT = '443'
 
 PRODUCTION_SERVER_NAME = 'jozihub.com'
 
+PRODUCTION_SERVER_NAMES = 'jozihub.com'
+
 DEPLOY_USER = getpass.getuser()
 
 USE_CELERY = True
+
+CONFIGURE_SETTINGS_LOCAL = True
+
+DB_TO_CONFIGURE = 'postgres'
+#DB_TO_CONFIGURE = 'mysql'
+
+CONFIGURE_DB = True
+
+SYNC_DB = CONFIGURE_DB
+
+CONFIGURE_RABBITMQ = True
+
+WEBSERVER_TO_CONFIGURE = 'nginx'
+#WEBSERVER_TO_CONFIGURE = 'apache'
+
+CONFIGURE_SUPERVISOR = True
+
+CONFIGURE_CERT = True
+
+RUN_MIGRATIONS = True
+
+RUN_TESTS = True
 
 DEV_SERVERS = ['ubuntu@precise.dev.unomena.net']
 
 QA_SERVERS = ['ubuntu@precise.qa.unomena.net']
 
 PROD_SERVERS = ['ubuntu@web1.prod.unomena.net', 'ubuntu@web2.prod.unomena.net']
+
+TESTS_TO_RUN = (
+    'app.authentication '
+    'tunobase.core '
+    'tunobase.blog '
+    'tunobase.commenting '
+    'tunobase.poll '
+    'tunobase.social_media.tunosocial'
+)
