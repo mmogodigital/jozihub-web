22011914e2134d95b6c214f498cd72800afc2ab6
Trevor <trevor@projectcodex.co>
2015-08-25 15:28:17 +0200

    Email test

--- src/app/authentication/tests.py
+++ src/app/authentication/tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
+from django.core import mail
+from django.test.utils import override_settings
 
-# Create your tests here.
 
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
+class EmailTest(TestCase):
 
-class SimpleTest(TestCase):
-    def test_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+    def test_send_email(self):
+        mail.send_mail('Copy for basic membership application',
+                       'Here is the message.',
+                       'info@jozihub.org', ['trevor@projectcodex.co'],
+                       fail_silently=True)
+        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(mail.outbox[0].subject,
+                          'Copy for basic membership application')
