eae44d552fcbcf3b66f0bd471aa88489302dea81
Trevor <trevor@projectcodex.co>
2015-08-27 15:49:33 +0200

    test_registration_view is modified

--- src/app/authentication/tests.py
+++ src/app/authentication/tests.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from registration import signals as registration_signals
 
 
-@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend', USE_CELERY=False)
 class EmailTest(TestCase):
 
     def test_send_email(self):
@@ -21,10 +21,18 @@ class EmailTest(TestCase):
     def test_registration_view(self):
         response = self.client.post(reverse('secure_register'),
                                     data={'username': 'foo',
-                                          'email': 'foo@example.com',
+                                          'first_name': 'foo',
+                                          'last_name': 'foo',
+                                          'email': 'foo@example2.com',
                                           'password1': 'foo',
                                           'password2': 'foo',
+                                          'field_of_expertise': 1,
+                                          'when_to_host_event': 1,
+                                          'information_about_jozihub': 1,
+                                          'when_to_get_access': 1,
+                                          'happy_with_the_price': 1,
                                           settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
+        print response
         self.assertEquals(len(mail.outbox), 1)
         self.assertEqual(response.status_code, 200)
         self.failUnless(response.context['form'])
