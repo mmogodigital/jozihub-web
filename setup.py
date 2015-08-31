3e5715f8c989523a2bede3308710bb8d0144057f
Simon de Haan <simon@praekeltfoundation.org>
2015-08-31 09:23:13 +0200

    pinning Django version

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     ],
     install_requires = [
         'South',
+        'django==1.6.5',
         'django-debug-toolbar==0.11.0',
         'django-countries',
         'django-polymorphic',
