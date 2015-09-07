afc860c80aaccefccdb9431363be79fe13daceff
Trevor <trevor@projectcodex.co>
2015-09-07 17:02:18 +0200

    fixing test_activation_view

--- src/app/authentication/tests.py
+++ src/app/authentication/tests.py
@@ -31,7 +31,23 @@ class EmailTest(TestCase):
                          reverse('registration_complete'))
 
     def test_activation_view(self):
+
+        response = self.client.post(reverse('secure_register'),
+                                    data={'username': 'foo',
+                                          'first_name': 'foo',
+                                          'last_name': 'foo',
+                                          'email': 'foo@example2.com',
+                                          'password1': 'foo',
+                                          'password2': 'foo',
+                                          'field_of_expertise': 1,
+                                          'when_to_host_event': 1,
+                                          'information_about_jozihub': 1,
+                                          'when_to_get_access': 1,
+                                          'happy_with_the_price': 1,
+                                          settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
+
         response = self.client.get(reverse('secure_activate',
-                                           kwargs={'activation_key': ProjectRegistrationProfile.objects.get(user=self.sample_user).activation_key}))
+                                           kwargs={'activation_key': ProjectRegistrationProfile.objects.get(user__username='foo').activation_key}))
+        print response
         self.assertEquals(len(mail.outbox), 1)
         self.assertEqual(response.status_code, 200)
