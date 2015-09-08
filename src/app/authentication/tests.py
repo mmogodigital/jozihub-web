087ee2c7ef15dfce99097072fb8dd3ca4f160f92
Milton Madanda <madandat@gmail.com>
2015-09-08 12:35:31 +0200

    fix tests

--- src/app/authentication/tests.py
+++ src/app/authentication/tests.py
@@ -23,7 +23,6 @@ class EmailTest(TestCase):
                                           'when_to_get_access': 1,
                                           'happy_with_the_price': 1,
                                           settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
-        print response
         self.assertEquals(len(mail.outbox), 1)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['Location'],
@@ -32,22 +31,27 @@ class EmailTest(TestCase):
 
     def test_activation_view(self):
 
-        response = self.client.post(reverse('secure_register'),
-                                    data={'username': 'foo',
-                                          'first_name': 'foo',
-                                          'last_name': 'foo',
-                                          'email': 'foo@example2.com',
-                                          'password1': 'foo',
-                                          'password2': 'foo',
-                                          'field_of_expertise': 1,
-                                          'when_to_host_event': 1,
-                                          'information_about_jozihub': 1,
-                                          'when_to_get_access': 1,
-                                          'happy_with_the_price': 1,
-                                          settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
+        response = self.client.post(
+            reverse('secure_register'),
+            data={'username': 'foo',
+                  'first_name': 'foo',
+                  'last_name': 'foo',
+                  'email': 'foo@example2.com',
+                  'password1': 'foo',
+                  'password2': 'foo',
+                  'field_of_expertise': 1,
+                  'when_to_host_event': 1,
+                  'information_about_jozihub': 1,
+                  'when_to_get_access': 1,
+                  'happy_with_the_price': 1,
+                  settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
 
-        response = self.client.get(reverse('secure_activate',
-                                           kwargs={'activation_key': ProjectRegistrationProfile.objects.get(user__username='foo').activation_key}))
-        print response
+        response = self.client.get(reverse('secure_activate', kwargs={
+            'activation_key': ProjectRegistrationProfile.objects.get(
+                user__email='foo@example2.com').activation_key}))
         self.assertEquals(len(mail.outbox), 1)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response['Location'],
+            'http://testserver%s' %
+            reverse('registration_activation_complete'))
