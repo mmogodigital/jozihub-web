f794319ff10eb56b831b6cf129aa3c6189ff879d
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-10-09 18:35:26 +0200

    added a url for the venue request view and link from the main nav

--- src/app/events/urls.py
+++ src/app/events/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 #from tunobase.corporate.media import views as media_views
-from app.events import views
+from app.events import views, forms
 
 
 urlpatterns = patterns('',
@@ -11,7 +11,13 @@ urlpatterns = patterns('',
         ),
         name='events'
     ),
-
+    url(r'^venue_hire/$', 
+        views.VenueHire.as_view(
+            form_class=forms.VenueHireForm,
+            template_name='events/venue_hire.html'
+        ),
+        name='venue_hire'
+    ),
     url(r'^(?P<slug>[-\w]+)/$',
         views.EventDetail.as_view(
             template_name='events/event_detail.html',
