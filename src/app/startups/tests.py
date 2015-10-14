3f99a8cb25f2b3c37086ef121ae5ac72efde17cc
TrevorOctober <trevor@projectcodex.co>
2015-10-14 12:52:33 +0200

    writing test for startup companies

--- src/app/startups/tests.py
+++ src/app/startups/tests.py
@@ -1,3 +1,27 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.core import mail
+from django.test.utils import override_settings
+from django.conf import settings
+from .models import StartupCompanies
 
-# Create your tests here.
+
+class EmailTest(TestCase):
+
+    # def test_startup_view(self):
+    #     response = self.client.get(reverse('startups'),
+    #                                 data={'name': 'foo',
+    #                                       'founder_photographs': 'foo',
+    #                                       'Link_to_their_website': 'foo.com',
+    #                                       'social_media_profiles': '@foo',
+    #                                       'contact_details': '0987654321',
+    #                                       settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(StartupCompanies.objects.count(), 1)
+
+    def test_startups(self):
+    	startup = StartupCompanies.objects.create(name="foo", founder_photographs="foo",
+    											  Link_to_their_website="foo.com", social_media_profiles="@foo",
+    											  contact_details="0987654321")
+    	self.assertEqual(startup.pk, 1)
+    	self.assertEqual(startup.name, 'foo')
