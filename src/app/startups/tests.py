56cebd93124b36e422d900b9c72a32d9aa196844
TrevorOctober <trevor@projectcodex.co>
2015-10-14 17:31:40 +0200

    startup companies test fail

--- src/app/startups/tests.py
+++ src/app/startups/tests.py
@@ -4,24 +4,23 @@ from django.core import mail
 from django.test.utils import override_settings
 from django.conf import settings
 from .models import StartupCompanies
+from .views import Startup
 
 
-class EmailTest(TestCase):
+class Startups(TestCase):
 
-    # def test_startup_view(self):
-    #     response = self.client.get(reverse('startups'),
-    #                                 data={'name': 'foo',
-    #                                       'founder_photographs': 'foo',
-    #                                       'Link_to_their_website': 'foo.com',
-    #                                       'social_media_profiles': '@foo',
-    #                                       'contact_details': '0987654321',
-    #                                       settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(StartupCompanies.objects.count(), 1)
+    def test_startup_view(self):
 
-    def test_startups(self):
-    	startup = StartupCompanies.objects.create(name="foo", founder_photographs="foo",
-    											  Link_to_their_website="foo.com", social_media_profiles="@foo",
-    											  contact_details="0987654321")
-    	self.assertEqual(startup.pk, 1)
-    	self.assertEqual(startup.name, 'foo')
+    	
+        response = self.client.post(reverse('console_startups_create'),
+                                    data={'name': 'foo',
+                                    	  'rich_content': 'some txt',
+                                          'founder_photographs': 'foo',
+                                          'Link_to_their_website': 'foo.com',
+                                          'social_media_profiles': '@foo',
+                                          'contact_details': '0987654321',
+                                          settings.HONEYPOT_FIELD_NAME: settings.HONEYPOT_VALUE})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(StartupCompanies.objects.count(), 1)
+  
+		
