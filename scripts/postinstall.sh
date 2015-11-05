cdd7e15eacf31139d942c75e07359fa65d0257c3
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-11-05 10:18:35 +0200

    added initial migrations for authentication

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,6 +4,7 @@ pip install http://github.com/unomena/django-ckeditor-new/tarball/3.6.2.2#egg=dj
 pip install http://github.com/unomena/django-photologue/tarball/2.8.praekelt#egg=django-photologue-2.8.praekelt
 pip install http://github.com/unomena/tunobase/tarball/1.0.9#egg=tunobase-1.0.9
 
+$manage migrate authentication 0001 --fake
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
 
