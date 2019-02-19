8c6ed2761fb197f2300ef112984a92e44e137b77
Codebender <tawanda@mmogodigital.com>
2019-02-19 12:20:25 +0200

    Changes

--- src/app/events/admin.py
+++
@@ -1,5 +0,0 @@
-'''
-Created on 25 Nov 2013
-
-@author: michael
-'''
--- src/app/startups/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from app.startups import models
-
-
-admin.site.register(models.StartupCompanies)
